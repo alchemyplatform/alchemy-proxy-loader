fdccd71e31c4328a4ca106b11989d48a3634fecb
Dan <dan.coombs@alchemy.com>
2026-01-06 15:30:30 -0600

    feat: add script to deploy from bytecode

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,98 +3,50 @@ pragma solidity ^0.8.13;
 
 import {AlchemyProxyLoader} from "../src/AlchemyProxyLoader.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 import {Script} from "forge-std/Script.sol";
-import {console} from "forge-std/console.sol";
+import {DeployConstants} from "./DeployConstants.sol";
 
-contract DeployScript is Script {
-    address public prodProxyLoaderOwner = 0xDdF32240B4ca3184De7EC8f0D5Aba27dEc8B7A5C;
-    address public expectedProdDeployAddress = 0x55954b89aA546269D8eDA34d7b97C948fab74a0D;
-    address public stgProxyLoaderOwner = 0x4F0cF2a1D11De183E989fF6287cc0973670f0583;
-    address public expectedStgDeployAddress = 0x4eb21516cdd2355CeD7a82018854BBe2E6Df018c;
+contract DeployScript is Script, DeployConstants {
     bool internal _isProd;
-
-    // Prod, 0.7 EP
-    bytes32 public proxySalt = 0x00000000000000000000000000000000000000005aff81b0e059e437fe647c08;
-    address public proxyAddress = 0x00000000000667F27D4DB42334ec11a25db7EBb4;
-
-    // Prod, 0.6 EP
-    bytes32 public proxySalt2 = 0x00000000000000000000000000000000000000001d26e644345a7729701286e0;
-    address public proxyAddress2 = 0x0000000000ce04e2359130e7d0204A5249958921;
-
-    // Staging, 0.7 EP
-    address public proxyAddress3 = 0x0AFc5739C3312Fc5B0900fD9CA4b4e4b2B2aD085;
-
-    // Staging, 0.6 EP
-    address public proxyAddress4 = 0xc4C4202DD4515678C9415d8Ebc8be116bd0d1828;
 
     function run() public {
         vm.startBroadcast();
 
         _isProd = vm.envBool("IS_PROD");
+        (
+            bytes32[4] storage deploySalts,
+            address[4] storage expectedProxyAddresses,
+            address loaderOwner,
+            address expectedDeployAddress
+        ) = getDeployConfig(_isProd);
 
-        if (_isProd) {
-            if (expectedProdDeployAddress.code.length == 0) {
-                address proxyLoader =
-                    address(new AlchemyProxyLoader{salt: 0}(0xDdF32240B4ca3184De7EC8f0D5Aba27dEc8B7A5C));
+        if (expectedDeployAddress.code.length == 0) {
+            address proxyLoader = address(new AlchemyProxyLoader{salt: 0}(loaderOwner));
 
-                if (proxyLoader != expectedProdDeployAddress) {
+            if (proxyLoader != expectedProdDeployAddress) {
+                revert(
+                    string(
+                        abi.encodePacked(
+                            "Proxy loader deployed to: ", proxyLoader, " instead of ", expectedProdDeployAddress
+                        )
+                    )
+                );
+            }
+        }
+
+        for (uint256 i = 0; i < deploySalts.length; i++) {
+            if (expectedProxyAddresses[i].code.length == 0) {
+                address proxy = address(new ERC1967Proxy{salt: deploySalts[i]}(expectedDeployAddress, ""));
+                if (proxy != expectedProxyAddresses[i]) {
                     revert(
                         string(
-                            abi.encodePacked(
-                                "Proxy loader deployed to: ", proxyLoader, " instead of ", expectedProdDeployAddress
-                            )
+                            abi.encodePacked("Proxy deployed to: ", proxy, " instead of ", expectedProxyAddresses[i])
                         )
                     );
-                }
-            }
-
-            if (proxyAddress.code.length == 0) {
-                address proxy = address(new ERC1967Proxy{salt: proxySalt}(expectedProdDeployAddress, ""));
-
-                if (proxy != proxyAddress) {
-                    revert(string(abi.encodePacked("Proxy1 deployed to: ", proxy, " instead of ", proxyAddress)));
-                }
-            }
-
-            if (proxyAddress2.code.length == 0) {
-                address proxy2 = address(new ERC1967Proxy{salt: proxySalt2}(expectedProdDeployAddress, ""));
-
-                if (proxy2 != proxyAddress2) {
-                    revert(string(abi.encodePacked("Proxy2 deployed to: ", proxy2, " instead of ", proxyAddress2)));
-                }
-            }
-        } else {
-            if (expectedStgDeployAddress.code.length == 0) {
-                address proxyLoader2 = address(new AlchemyProxyLoader{salt: 0}(stgProxyLoaderOwner));
-
-                if (proxyLoader2 != expectedStgDeployAddress) {
-                    revert(
-                        string(
-                            abi.encodePacked(
-                                "Proxy loader 2 deployed to: ", proxyLoader2, " instead of ", stgProxyLoaderOwner
-                            )
-                        )
-                    );
-                }
-            }
-
-            if (proxyAddress3.code.length == 0) {
-                address proxy3 = address(new ERC1967Proxy{salt: 0}(expectedStgDeployAddress, ""));
-
-                if (proxy3 != proxyAddress3) {
-                    revert(string(abi.encodePacked("Proxy3 deployed to: ", proxy3, " instead of ", proxyAddress3)));
-                }
-            }
-
-            if (proxyAddress4.code.length == 0) {
-                address proxy4 = address(new ERC1967Proxy{salt: bytes32(uint256(1))}(expectedStgDeployAddress, ""));
-
-                if (proxy4 != proxyAddress4) {
-                    revert(string(abi.encodePacked("Proxy4 deployed to: ", proxy4, " instead of ", proxyAddress4)));
                 }
             }
         }
+
         vm.stopBroadcast();
     }
 }
--- test/AlchemyProxyLoader.t.sol
+++ test/AlchemyProxyLoader.t.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {AlchemyProxyLoader} from "../src/AlchemyProxyLoader.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
+import {Create2Deployer} from "../src/Create2Deployer.sol";
 
 contract AlchemyProxyLoaderTest is Test {
     AlchemyProxyLoader public proxyRegistry;
@@ -64,7 +65,7 @@ contract AlchemyProxyLoaderTest is Test {
         assertEq(predicted, 0x55954b89aA546269D8eDA34d7b97C948fab74a0D);
     }
 
-    function test_getDeployedProxyInitcode() public {
+    function test_getDeployedProxyInitcodeProd() public {
         vm.startPrank(CREATE2_FACTORY);
 
         bytes32 bytecodeHash = keccak256(
@@ -88,5 +89,73 @@ contract AlchemyProxyLoaderTest is Test {
             )
         );
         assertEq(proxy2, 0x0000000000ce04e2359130e7d0204A5249958921);
+
+        address proxy5 = address(
+            new ERC1967Proxy{salt: 0x000000000000000000000000000000000000000031c9751e6d4c2d0bd4b5c0a8}(
+                address(0x55954b89aA546269D8eDA34d7b97C948fab74a0D), ""
+            )
+        );
+        assertEq(proxy5, 0x000000000804FF442BE7d200445362c46A008ADd);
+
+        address proxy7 = address(
+            new ERC1967Proxy{salt: 0x0000000000000000000000000000000000000000000000000000000000433709}(
+                address(0x55954b89aA546269D8eDA34d7b97C948fab74a0D), ""
+            )
+        );
+        assertEq(proxy7, 0xA52A44684E57dcb75Ca416B64E7C5466FaaEb1CA);
+    }
+
+    function test_getDeployedProxyInitcodeStg() public {
+        vm.startPrank(CREATE2_FACTORY);
+
+        bytes32 bytecodeHash = keccak256(
+            abi.encodePacked(
+                type(ERC1967Proxy).creationCode, abi.encode(address(0x4eb21516cdd2355CeD7a82018854BBe2E6Df018c), "")
+            )
+        );
+
+        assertEq(bytecodeHash, 0x62b6a1a799713d52d22b633a6a432fc72922df1432fcc646c2601d8c2f9dd981);
+        new AlchemyProxyLoader{salt: 0}(address(0x4F0cF2a1D11De183E989fF6287cc0973670f0583));
+
+        address proxy3 = address(
+            new ERC1967Proxy{salt: bytes32(uint256(0))}(address(0x4eb21516cdd2355CeD7a82018854BBe2E6Df018c), "")
+        );
+        assertEq(proxy3, 0x0AFc5739C3312Fc5B0900fD9CA4b4e4b2B2aD085);
+
+        address proxy4 = address(
+            new ERC1967Proxy{salt: bytes32(uint256(1))}(address(0x4eb21516cdd2355CeD7a82018854BBe2E6Df018c), "")
+        );
+        assertEq(proxy4, 0xc4C4202DD4515678C9415d8Ebc8be116bd0d1828);
+
+        address proxy6 = address(
+            new ERC1967Proxy{salt: bytes32(uint256(2))}(address(0x4eb21516cdd2355CeD7a82018854BBe2E6Df018c), "")
+        );
+        assertEq(proxy6, 0x813d0b40CF2022Ccaf7451220abe7c7Efb01D0EE);
+
+        address proxy8 = address(
+            new ERC1967Proxy{salt: bytes32(uint256(3))}(address(0x4eb21516cdd2355CeD7a82018854BBe2E6Df018c), "")
+        );
+        assertEq(proxy8, 0x69FFF4542cD2BaA91D6a797e5ebD9b07C88BdE70);
+    }
+
+    function test_deployProxyFromBytecodeProd() public {
+        // First deploy the AlchemyProxyLoader
+        string memory loaderBytecode = vm.readFile("bytecode/AlchemyProxyLoader.bin");
+        bytes memory loaderCode = vm.parseBytes(loaderBytecode);
+        bytes memory loaderArgs = abi.encode(address(0xDdF32240B4ca3184De7EC8f0D5Aba27dEc8B7A5C));
+        address computedLoader = Create2Deployer.computeAddress(loaderCode, loaderArgs, bytes32(uint256(0)));
+        address loader = Create2Deployer.deploy(loaderCode, loaderArgs, bytes32(uint256(0)));
+        assertEq(computedLoader, loader);
+        assertEq(loader, 0x55954b89aA546269D8eDA34d7b97C948fab74a0D);
+
+        // Now deploy the proxy
+        string memory hexBytecode = vm.readFile("bytecode/ERC1967Proxy.bin");
+        bytes memory bytecode = vm.parseBytes(hexBytecode);
+        bytes memory constructorArgs = abi.encode(address(0x55954b89aA546269D8eDA34d7b97C948fab74a0D), "");
+        bytes32 salt = 0x00000000000000000000000000000000000000005aff81b0e059e437fe647c08;
+        address computedProxy = Create2Deployer.computeAddress(bytecode, constructorArgs, salt);
+        address proxy = Create2Deployer.deploy(bytecode, constructorArgs, salt);
+        assertEq(computedProxy, proxy);
+        assertEq(proxy, 0x00000000000667F27D4DB42334ec11a25db7EBb4);
     }
 }
